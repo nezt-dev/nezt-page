49733505fa37ba7626f0a3e3bdb13e0ce5a72490
Miguel Fasanella <miguellfasanellap@gmail.com>
2022-09-14 20:13:35 -0400

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { lightTheme } from '../themes';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={ lightTheme }>
+      {/* <CssBaseline /> */}
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
